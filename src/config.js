dc2829f2388c645e2501d3cb9dc5f08d3ab348ab
LuisGT <luis.gomez.trujillo@gmail.com>
2026-02-11 12:22:41 -0500

    FIX: Update backend URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // IMPORTANTE: Sin la barra "/" al final
 // const API_URL = "https://tu-backend-en-render.onrender.com";
 
-const API_URL ="https://backend-loteria-lqf7.onrender.com";
+const API_URL ="https://backend-loteria-920p.onrender.com";
+
 
 export default API_URL;
