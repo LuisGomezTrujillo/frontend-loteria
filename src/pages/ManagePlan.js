1266e974dd082d2c4ca2b12622c0f004e351ae91
LuisGT <luis.gomez.trujillo@gmail.com>
2026-02-15 19:35:50 -0500

    FIX: Delete Navigate ESLint Warning

--- src/pages/ManagePlan.js
+++ src/pages/ManagePlan.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import API_URL from '../config';
 
 const ManagePlan = () => {
-  const navigate = useNavigate();
+  
   
   // Estados para CRUD de Planes
   const [planes, setPlanes] = useState([]);
